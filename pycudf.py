00812725d1f7e4f9b578c96f296b1e269c13a68c
guandong <1623088350@qq.com>
2023-02-16 18:16:00 +0800

    Update pycudf.py

--- pycudf.py
+++ pycudf.py
@@ -3,6 +3,9 @@ import glob
 import os
 import re
 
+
+# 所有连续 N 个字，和它出现的总次数
+dic_NGrams = {}
 NGram = 5
 
 def NG(strs, NGram):
@@ -27,12 +30,12 @@ def NG(strs, NGram):
     return gss
 
 paths = glob.glob('./**/OPS/*.html', recursive=True)
+paths = [ pth for pth in paths if os.stat(pth).st_size >= 1024 * 10 ]  # 小于 10k 不是正文，跳过
 
+paths = paths[0:2]
+
+curr = 0
 for pth in paths:
-    size = os.stat(pth).st_size
-    if size < 1024 * 10:
-        continue  # 小于 10k 不是正文，跳过
-    
     with open(pth, encoding='utf-8-sig') as f:
         data = f.read() #f.readlines()
         lines = re.compile(r'<p>(.+?)<\/p>').findall(data)
@@ -42,7 +45,40 @@ for pth in paths:
         for ar in sentences:
             for sent in ar:
                ng = NG(sent, NGram)
+               for g in ng:
+                    k = f'{len(g)}'
+                    if not k in dic_NGrams:
+                        dic_NGrams[k] = {}
+                    if not g in dic_NGrams[k]:
+                        dic_NGrams[k][g] = {}
+                    if not 'n' in dic_NGrams[k][g]:
+                        dic_NGrams[k][g]['n'] = 0  # 该词出现次数
+                    
+                    dic_NGrams[k][g]['n'] += 1
+        curr += 1
+        print(f'building dic_NGrams from html: {curr} / {len(paths)}')
 
-        a = 1
+
+    # 算每个词或单个字真实出现概率（简称真实概率）、理论完全随机出现的概率（简称理论概率）
+
+    # 先算所有词的真实概率
+
+    for n in range(1, NGram+1):
+        ng = dic_NGrams[f'{n}']
+        total = len(list(ng.keys()))
+
+        # for (let [k, v] of Object.entries(ng)) {
+        #     let times = v[`n`]           // 出现次数
+        #     let real_p = times / total   // 真实概率
+        #     v[`real_p`] = real_p.toFixed(6)
+        # }
+
+        # console.log(`calc real_p ${n} / ${NGram}`)
+
+    
+
+
+
+a = 1
 
 
